a9edcf440e9b777ba500a6d7ec917db83802fb11
02akshay123 <akshaymjadhav96@gmail.com>
2022-04-08 23:43:07 +0530

    Second commit
    here is my second program

---
+++ DecEveGITHUBGang/src/groupD/DEC21.java
@@ -0,0 +1,12 @@
+package groupD;
+
+public class DEC21 {
+	public static void main(String[]args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+	}
+
+}
