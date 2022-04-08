53fcc192c2f345c0bd8756b8eb1706e5195135b8
02akshay123 <akshaymjadhav96@gmail.com>
2022-04-08 23:22:07 +0530

    First commit
    Here is my first feature please review it.

---
+++ DecEveGITHUBGang/src/groupD/Member.java
@@ -0,0 +1,12 @@
+package groupD;
+
+public class Member {
+	public static void main(String[]args) {
+		System.out.println("Akshay");
+		System.out.println("Aakash");
+		System.out.println("Mahesh");
+		System.out.println("Bhakti");
+		System.out.println("Snehal");
+	}
+
+}
